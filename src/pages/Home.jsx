e3ddc9ca86c45f09e9650765e86686dd1640999a
aksinghania <akshtsinghnia@gmail.com>
2024-06-28 01:26:43 +0530

    imported the bird and the plane

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP 
@@ -39,11 +43,15 @@ const Home = () => {
             intensity={1}
           />
 
+          <Bird />
+          <Sky />
+
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
           />
+          <Plane />
         </Suspense>
       </Canvas>
     </section>
