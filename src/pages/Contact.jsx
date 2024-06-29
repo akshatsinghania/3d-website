f9f92e878b9a4c67d495201e97f158b1c74c21b7
aksinghania <akshtsinghnia@gmail.com>
2024-06-29 10:57:50 +0530

    partially added forms component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+
+  const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  return (
+    <section className="relative flex lg:flex-row flex-col max-conatiner">
+      <div className="flex-1 min-w-[50%] flex flex-col ">
+        <h1 className="head-text">Get in touch</h1>
+
+        <form className="w-full flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Akshat"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="singhaniaakshat1@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows={4}
+              className="textarea"
+              placeholder="Let me know how i can help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
