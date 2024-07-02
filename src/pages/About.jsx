19320c348f0d78d4ec2db24f4cb88dc738c61cba
aksinghania <akshtsinghnia@gmail.com>
2024-07-02 07:49:53 +0530

    partialyy added about and project page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Akshat
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Software Engineer based in India, speciaizing in technical education
+          through hands on learning and building applications
+        </p>
+        <div></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
